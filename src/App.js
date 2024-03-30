f49b7a87fcb6727df1614ad9b97ee87df4472507
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-03-31 00:44:39 +0900

    Feat: 정답 만드는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+function RandomAnswer() {
+  const CORRECT_ANSWER = [];
+
+  while (CORRECT_ANSWER.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!CORRECT_ANSWER.includes(number)) {
+      CORRECT_ANSWER.push(number);
+    }
+  }
+  return CORRECT_ANSWER;
+}
+
+const game = new App();
+game.play();
 
 export default App;
