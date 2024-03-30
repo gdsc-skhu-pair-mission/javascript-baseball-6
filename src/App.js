82a707d12ada5f710a51d9e501745c88f5996d21
Younghyun Kim <dana_03@naver.com>
2024-03-31 01:40:42 +0900

    Feat: 사용자로부터 입력받는 수 검증 구현

--- src/App.js
+++ src/App.js
@@ -18,6 +18,30 @@ function RandomAnswer() {
   return CORRECT_ANSWER;
 }
 
+async function InputAnswer() {
+  let userInput;
+  let userAnswer;
+
+  userInput = await Console.print('숫자를 입력해주세요 : ');
+  if (isNaN(parseInt(userInput))) {
+    throw new Error('숫자가 아닌 것을 입력하시면 안됩니다.');
+  }
+  if (userInput.length !== 3) {
+    throw new Error('3자리 수를 입력해주세요.');
+  }
+
+  userAnswer = Array.from(userInput).map(Number);
+
+  if (new Set(userAnswer).size !== userAnswer.length) {
+    throw new Error('중복된 수를 입력하시면 안됩니다.');
+  }
+  if (userAnswer.some(number => number < 1 || number > 9)) {
+    throw new Error('1 ~ 9 사이의 수를 입력해주세요.');
+  }
+
+  return userAnswer;
+}
+
 const game = new App();
 game.play();
 
