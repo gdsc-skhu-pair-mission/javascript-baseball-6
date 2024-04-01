88d435325efab90e1fd94f5c4baea8670452dd3b
SEYEON-PARK <fourleaf123@naver.com>
2024-04-01 17:07:38 +0900

    final

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { startGame } from "./async.js";
 
 class App {
   async play() {
-    // await startGame(); // startGame 함수가 완료될 때까지 기다림
+    await startGame(); // startGame 함수가 완료될 때까지 기다림
     // console.log("게임이 종료되었습니다."); // 게임 종료 후에 실행될 코드 추가
   }
 }
 
-startGame();
+// startGame();
+const app = new App();
+app.play();
 
 export default App;
--- src/async.js
+++ src/async.js
@@ -1,39 +1,48 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 export { startGame };
 
-const COMPUTER = [];
-while (COMPUTER.length < 3) {
-  const number = Random.pickNumberInRange(1, 9);
-  if (!COMPUTER.includes(number)) {
-    COMPUTER.push(number);
+function generateRandomNumber() {
+  const COMPUTER = [];
+  while (COMPUTER.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!COMPUTER.includes(number)) {
+      COMPUTER.push(number);
+    }
   }
+  console.log(COMPUTER);
+  return COMPUTER;
 }
 
 Console.print("숫자 야구 게임을 시작합니다.");
 
 async function getUserInput() {
-  try {
-    let inputNumber = [];
-    let USER_INPUT = await Console.readLineAsync("숫자를 입력해주세요: ");
+  // try {
+  let inputNumber = [];
+  let USER_INPUT = await Console.readLineAsync("숫자를 입력해주세요: ");
 
-    if (isNaN(USER_INPUT) || USER_INPUT.length != 3) {
-      throw new Error("잘못된 값입니다.");
-    }
-
-    inputNumber.push(parseInt(USER_INPUT / 100));
-    USER_INPUT = USER_INPUT % 100;
-    inputNumber.push(parseInt(USER_INPUT / 10));
-    USER_INPUT = USER_INPUT % 10;
-    inputNumber.push(parseInt(USER_INPUT));
-
-    return inputNumber;
-  } catch (err) {
-    Console.print(err.message);
-    return getUserInput(); // process.exit() 안 쓰고 어떻게 처리해야 하는지 모르겠다.
+  if (isNaN(USER_INPUT) || USER_INPUT.length != 3 || USER_INPUT < 0) {
+    throw new Error("[ERROR] 잘못 입력하셨습니다.");
   }
+
+  inputNumber.push(parseInt(USER_INPUT / 100));
+  USER_INPUT = USER_INPUT % 100;
+  inputNumber.push(parseInt(USER_INPUT / 10));
+  USER_INPUT = USER_INPUT % 10;
+  inputNumber.push(parseInt(USER_INPUT));
+
+  if (new Set(inputNumber).size !== 3) {
+    throw new Error("[ERROR] 숫자를 중복하여 입력하셨습니다.");
+  }
+
+  return inputNumber;
+  // } catch (err) {
+  // Console.print(err.message);
+  // return getUserInput(); // process.exit() 안 쓰고 어떻게 처리해야 하는지 모르겠다.
+  // }
 }
 
 async function startGame() {
+  let COMPUTER = generateRandomNumber();
   while (true) {
     let inputNumber = await getUserInput();
     let strike = 0;
@@ -57,12 +66,13 @@ async function startGame() {
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
       );
       if (answer === "1") {
+        COMPUTER = generateRandomNumber();
         continue;
       } else if (answer === "2") {
         break;
       } else {
-        Console.print("잘못된 숫자를 입력하셨습니다.");
-        break;
+        throw new Error("[ERROR]");
+        // break;
       }
     } else if (ball === 0 && strike === 0) {
       Console.print("낫싱");
