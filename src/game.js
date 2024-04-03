b890f3c048067f749ef8b922c32fc76e470bdefb
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-04-04 03:03:09 +0900

    refactor: 1차 리팩토링

--- src/game.js
+++ src/game.js
@@ -1,89 +1,91 @@
 import { Random, Console } from "@woowacourse/mission-utils";
-export { startGame };
+import { NUM_DIGITS } from "./constants.js";
+export { playGame };
 
-function generateRandomNumber() {
-  const COMPUTER = [];
-  while (COMPUTER.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
-    if (!COMPUTER.includes(number)) {
-      COMPUTER.push(number);
+const generateRandomNumber = () => {
+  const RANDOM_NUMBER = [];
+  while (RANDOM_NUMBER.length < NUM_DIGITS) {
+    const GENERATE_NUMBER = Random.pickNumberInRange(1, 9);
+    if (!RANDOM_NUMBER.includes(GENERATE_NUMBER)) {
+      RANDOM_NUMBER.push(GENERATE_NUMBER);
     }
   }
-  console.log(COMPUTER);
-  return COMPUTER;
-}
+  return RANDOM_NUMBER;
+};
 
-Console.print("숫자 야구 게임을 시작합니다.");
-
-async function getUserInput() {
-  // try {
+const getUserInput = async () => {
   let inputNumber = [];
-  let USER_INPUT = await Console.readLineAsync("숫자를 입력해주세요: ");
-
-  if (isNaN(USER_INPUT) || USER_INPUT.length != 3 || USER_INPUT < 0) {
-    throw new Error("[ERROR] 잘못 입력하셨습니다.");
-  }
-
-  inputNumber.push(parseInt(USER_INPUT / 100));
-  USER_INPUT = USER_INPUT % 100;
-  inputNumber.push(parseInt(USER_INPUT / 10));
-  USER_INPUT = USER_INPUT % 10;
-  inputNumber.push(parseInt(USER_INPUT));
-
-  if (new Set(inputNumber).size !== 3) {
-    throw new Error("[ERROR] 숫자를 중복하여 입력하셨습니다.");
-  }
-
+  let user_input = await Console.readLineAsync("숫자를 입력해주세요: ");
+  checkUserInputNumber(inputNumber, user_input);
   return inputNumber;
-  // } catch (err) {
-  // Console.print(err.message);
-  // return getUserInput(); // process.exit() 안 쓰고 어떻게 처리해야 하는지 모르겠다.
-  // }
-}
+};
 
-async function startGame() {
-  let COMPUTER = generateRandomNumber();
+const checkUserInputNumber = async (inputNumber, user_input) => {
+  const CHECK_USER_INPUT = /^[0-9]+$/;
+
+  if (!CHECK_USER_INPUT.test(user_input)) {
+    throw new Error("[ERROR] 잘못된 입력입니다.");
+  } else {
+    for (const INPUT_NUM of user_input) {
+      inputNumber.push(Number(INPUT_NUM));
+    }
+    if (user_input.length !== NUM_DIGITS) {
+      throw new Error("[ERROR] 3자리 수를 입력해주세요.");
+    }
+    if (new Set(inputNumber).size !== NUM_DIGITS) {
+      throw new Error("[ERROR] 중복된 입력입니다.");
+    }
+  }
+};
+const playGame = async () => {
+  Console.print("숫자 야구 게임을 시작합니다.");
+  const RANDOM_NUMBER = generateRandomNumber();
   while (true) {
     let inputNumber = await getUserInput();
     let strike = 0;
     let ball = 0;
 
-    for (let i = 0; i < 3; i++) {
-      if (COMPUTER.includes(inputNumber[i])) {
-        if (inputNumber[i] === COMPUTER[i]) {
+    for (let i = 0; i < NUM_DIGITS; i++) {
+      if (RANDOM_NUMBER.includes(inputNumber[i])) {
+        if (inputNumber[i] === RANDOM_NUMBER[i]) {
           strike++;
         } else {
           ball++;
         }
       }
     }
-
-    if (strike === 3) {
+    if (strike === NUM_DIGITS) {
       Console.print("3스트라이크!");
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      askForRePlay();
+      break;
+    }
+    checkGameScore(strike, ball);
+  }
+};
 
-      let answer = await Console.readLineAsync(
-        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
-      );
-      if (answer === "1") {
-        COMPUTER = generateRandomNumber();
-        continue;
-      } else if (answer === "2") {
-        break;
-      } else {
-        throw new Error("[ERROR]");
-        // break;
-      }
-    } else if (ball === 0 && strike === 0) {
-      Console.print("낫싱");
+const checkGameScore = (strike, ball) => {
+  if (ball === 0 && strike === 0) {
+    Console.print("낫싱");
+  } else {
+    if (ball > 0 && strike === 0) {
+      Console.print(`${ball}볼`);
+    } else if (strike > 0 && ball === 0) {
+      Console.print(`${strike}스트라이크`);
     } else {
-      if (ball > 0 && strike === 0) {
-        Console.print(`${ball}볼 `);
-      } else if (strike > 0 && ball === 0) {
-        Console.print(`${strike}스트라이크`);
-      } else {
-        Console.print(`${ball}볼 ${strike}스트라이크`);
-      }
+      Console.print(`${ball}볼 ${strike}스트라이크`);
     }
   }
-}
+};
+
+const askForRePlay = async () => {
+  const ANSWER = await Console.readLineAsync(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. "
+  );
+  if (ANSWER === "1") {
+    await playGame();
+  } else if (ANSWER === "2") {
+  } else {
+    throw new Error("[ERROR]");
+  }
+};
