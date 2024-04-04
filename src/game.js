01ca3c865b259fe60d2b68f0b174aa56f4ad4cc2
SEYEON-PARK <fourleaf123@naver.com>
2024-04-04 13:12:53 +0900

    변수 이름 변경

--- src/game.js
+++ src/game.js
@@ -1,16 +1,17 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 export { startGame };
+const FIT_LENGTH = 3; // 맞는 길이!
 
 function generateRandomNumber() {
-  const COMPUTER = [];
-  while (COMPUTER.length < 3) {
+  const computerNumber = [];
+  while (computerNumber.length < 3) {
     const number = Random.pickNumberInRange(1, 9);
-    if (!COMPUTER.includes(number)) {
-      COMPUTER.push(number);
+    if (!computerNumber.includes(number)) {
+      computerNumber.push(number);
     }
   }
-  console.log(COMPUTER);
-  return COMPUTER;
+  console.log(computerNumber);
+  return computerNumber;
 }
 
 Console.print("숫자 야구 게임을 시작합니다.");
@@ -20,7 +21,7 @@ async function getUserInput() {
   let inputNumber = [];
   let USER_INPUT = await Console.readLineAsync("숫자를 입력해주세요: ");
 
-  if (isNaN(USER_INPUT) || USER_INPUT.length != 3 || USER_INPUT < 0) {
+  if (isNaN(USER_INPUT) || USER_INPUT.length != FIT_LENGTH || USER_INPUT < 0) {
     throw new Error("[ERROR] 잘못 입력하셨습니다.");
   }
 
@@ -42,15 +43,15 @@ async function getUserInput() {
 }
 
 async function startGame() {
-  let COMPUTER = generateRandomNumber();
+  let computerNumber = generateRandomNumber();
   while (true) {
     let inputNumber = await getUserInput();
     let strike = 0;
     let ball = 0;
 
-    for (let i = 0; i < 3; i++) {
-      if (COMPUTER.includes(inputNumber[i])) {
-        if (inputNumber[i] === COMPUTER[i]) {
+    for (let i = 0; i < FIT_LENGTH; i++) {
+      if (computerNumber.includes(inputNumber[i])) {
+        if (inputNumber[i] === computerNumber[i]) {
           strike++;
         } else {
           ball++;
@@ -66,7 +67,7 @@ async function startGame() {
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
       );
       if (answer === "1") {
-        COMPUTER = generateRandomNumber();
+        computerNumber = generateRandomNumber();
         continue;
       } else if (answer === "2") {
         break;
