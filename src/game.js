7dcdec90f8f21d1d5bbcfe9e20a973fbc3d7d473
SEYEON-PARK <fourleaf123@naver.com>
2024-04-04 13:19:52 +0900

    if문 간단히 변경

--- src/game.js
+++ src/game.js
@@ -78,13 +78,16 @@ async function startGame() {
     } else if (ball === 0 && strike === 0) {
       Console.print("낫싱");
     } else {
-      if (ball > 0 && strike === 0) {
-        Console.print(`${ball}볼 `);
-      } else if (strike > 0 && ball === 0) {
-        Console.print(`${strike}스트라이크`);
-      } else {
-        Console.print(`${ball}볼 ${strike}스트라이크`);
-      }
+      //if (ball > 0 && strike === 0) {
+      Console.print(
+        (ball > 0 ? `${ball}볼 ` : "") +
+          (strike > 0 ? `${strike}스트라이크` : "")
+      );
+      //} else if (strike > 0 && ball === 0) {
+      //   Console.print(`${strike}스트라이크`);
+      // } else {
+      //   Console.print(`${ball}볼 ${strike}스트라이크`);
+      // }
     }
   }
 }
